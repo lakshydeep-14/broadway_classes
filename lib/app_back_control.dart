7f34b4e57b19834768174f803cc7709a66ff22f9
lakshydeep_14 <lvikramsah@gmail.com>
2025-12-16 19:08:36 +0545

    class 9: back gesture added

--- lib/app_back_control.dart
+++ lib/app_back_control.dart
@@ -1,47 +1,46 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class AppBackControl extends StatefulWidget {
-//   const AppBackControl({required this.child, super.key, this.onBack});
+class AppBackControl extends StatefulWidget {
+  const AppBackControl({required this.child, super.key, this.onBack});
 
-//   final Widget child;
-//   final VoidCallback? onBack;
+  final Widget child;
+  final VoidCallback? onBack;
 
-//   @override
-//   State<AppBackControl> createState() => _AppBackControlState();
-// }
+  @override
+  State<AppBackControl> createState() => _AppBackControlState();
+}
 
-// class _AppBackControlState extends State<AppBackControl> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return PopScope(
-//       canPop: false, // Disable default back behavior
-//       onPopInvoked: (didPop) {
-//         FocusScope.of(context).unfocus();
-//         if (didPop) return;
-//         print('a');
-//         if (widget.onBack != null) {
-//           print('a');
-//           widget.onBack!.call();
-//         } else {
-//           Get.back();
-//           // Navigator.of(context).maybePop(); // default pop
-//         }
-//       },
-//       child: GestureDetector(
-//         onHorizontalDragUpdate: (details) {
-//           FocusScope.of(context).unfocus();
-//           if (Platform.isIOS && details.delta.dx > 10) {
-//             if (widget.onBack != null) {
-//               widget.onBack!.call();
-//             } else {
-//               Get.back();
-//             }
-//           }
-//         },
-//         child: widget.child,
-//       ),
-//     );
-//   }
-// }
+class _AppBackControlState extends State<AppBackControl> {
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false, // Disable default back behavior
+      onPopInvoked: (didPop) {
+        FocusScope.of(context).unfocus();
+        if (didPop) return;
+        print('a');
+        if (widget.onBack != null) {
+          print('a');
+          widget.onBack!.call();
+        } else {
+          // Navigator.of(context).maybePop(); // default pop
+        }
+      },
+      child: GestureDetector(
+        onHorizontalDragUpdate: (details) {
+          FocusScope.of(context).unfocus();
+          if (Platform.isIOS && details.delta.dx > 10) {
+            if (widget.onBack != null) {
+              widget.onBack!.call();
+            } else {
+              // Use you own function along with back.
+            }
+          }
+        },
+        child: widget.child,
+      ),
+    );
+  }
+}
